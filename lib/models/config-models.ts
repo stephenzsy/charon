fd62f6ab0a0308f0a9036b41e0f19a001a3d36b8
Stephen <stephen.zsy@gmail.com>
2015-11-23 03:04:46 -0800

    Initial checkin

---
+++ lib/models/config-models.ts
@@ -0,0 +1,12 @@
+export interface EapTlsModel {
+
+}
+
+export interface EapPeapModel {
+
+}
+
+export interface EapModel {
+	tls: EapTlsModel;
+	peap: EapPeapModel;
+}
---
+++ scripts/configure-freeradius.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+import {EapModel} from 'lib/models/config-models';
+
+class Configurator {
+	public static configureEap(eapModel: EapModel) {
+		
+	}
+}
+
+Configurator.configureEap({
+	tls: {},
+	peap: {}
+});
