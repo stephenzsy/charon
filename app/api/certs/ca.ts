1da52cb142ca968e810509ba675fefda23f627ee
Stephen <stephen.zsy@gmail.com>
2015-12-08 10:08:46 -0800

    fix getCertCA call

--- app/api/certs/ca.ts
+++ app/api/certs/ca.ts
@@ -1,13 +1,37 @@
 'use strict';
 
+import * as Q from 'q';
+import * as child_process from 'child_process';
 import * as path from 'path';
 import * as express from 'express';
+import {RequestEventHandlerFactory} from '../../../lib/event/event-handler';
 import {CaCertManager} from '../../../lib/certs/ca-cert-manager';
 
 var router: express.Router = express.Router();
 var caCertManager: CaCertManager = new CaCertManager(require('../../../config/ca.json'));
 
-export var getCertsCaHandler: express.RequestHandler = function(req: express.Request, res: express.Response): any {
-  console.log("OK");
-  res.send(caCertManager.certPath);
+interface GetCertsCaRequest { }
+
+interface GetCertsCaResult {
+  certificateInfoText: string;
 }
+
+class CertsCaRequestHandlerFactory extends RequestEventHandlerFactory<GetCertsCaRequest, GetCertsCaResult>{
+  protected getRequest(expressReq: express.Request): GetCertsCaRequest {
+    return {};
+  }
+
+  protected handleAsync(req: GetCertsCaRequest): Q.Promise<GetCertsCaResult> {
+    return Q.nfcall(child_process.execFile, 'openssl', [
+      'x509',
+      '-in', caCertManager.certPath,
+      '-text',
+      '-noout'], null).then((stdout: string): GetCertsCaResult=> {
+      return {
+        certificateInfoText: stdout
+      };
+    });
+  }
+}
+
+export var getCertsCaHandler: express.RequestHandler = (new CertsCaRequestHandlerFactory()).handler;
---
+++ lib/event/event-handler.ts
@@ -0,0 +1,38 @@
+///<reference path="../../typings/q/Q.d.ts"/>
+
+import * as express from 'express';
+import * as Q from 'q';
+
+export interface Event<TRequest, TResult> {
+  originalRequest: express.Request;
+  originalResponse: express.Response;
+  req?: TRequest;
+  res?: TResult;
+}
+
+export abstract class RequestEventHandlerFactory<TRequest, TResult> {
+
+  protected abstract getRequest(expressReq: express.Request): TRequest;
+  protected abstract handleAsync(req: TRequest): Q.Promise<TResult>;
+
+  private createEvent(req: express.Request, res: express.Response): Event<TRequest, TResult> {
+    return {
+      originalRequest: req,
+      originalResponse: res
+    };
+  }
+
+  get handler(): express.RequestHandler {
+    return (req: express.Request, res: express.Response) => {
+      var event: Event<TRequest, TResult> = this.createEvent(req, res);
+      event.req = this.getRequest(req);
+      this.handleAsync(event.req).then((result: TResult) => {
+        event.res = result;
+        event.originalResponse.send(result);
+      }, (err: any) => {
+          console.error(err);
+          event.originalResponse.sendStatus(500);
+        });
+    };
+  }
+}
